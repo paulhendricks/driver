278c048c37339a565a7520704e4ad16c5c2528d7
ironholds <ironholds@gmail.com>
2015-02-15 23:11:53 -0500

    move simplifiers into utilities.R

--- R/utilities.R
+++ R/utilities.R
@@ -34,3 +34,29 @@ driver_connect <- function(id, secret){
                           scope = "https://www.googleapis.com/auth/drive", cache = FALSE)
   return(token)
 }
+
+#Simplifiers
+simplify_response <- function(x){
+  UseMethod("simplify_response", x)
+}
+
+file_simp <- function(x){
+  x$labels <- unlist(x$labels)
+  x$exportLinks <- unlist(x$exportLinks)
+  x$userPermission <- unlist(x$userPermission)
+  x$ownerNames <- unlist(x$ownerNames)
+  return(x)
+}
+
+simplify_response.file_list <- function(x){
+  if(length(x$items) == 1){
+    x$items <- file_simp(x$items[[1]])
+  } else {
+    x$items <- lapply(x$items, file_simp)
+  }
+  return(x)
+}
+
+simplify_response.file_metadata <- function(x){
+  return(file_simp(x))
+}
