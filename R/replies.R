9214f56bccb866c6db48be4e8d5db779a2c5ea50
ironholds <ironholds@gmail.com>
2015-02-21 09:34:08 -0500

    delete_reply docs

--- R/replies.R
+++ R/replies.R
@@ -1,3 +1,22 @@
+#'@title delete a reply to a comment in a Google Drive file
+#'@description when provided with the relevant file, comment and reply IDs, \code{\link{delete_reply}}
+#'allows you to trash a reply. Note that this is irrevocable.
+#'
+#'@param token a token, generated with \code{\link{driver_connect}}.
+#'
+#'@param file_id the ID of a file - see \code{\link{file_metadata}} for further discussion.
+#'
+#'@param comment_id the ID of a comment, which can be easily retrieved with \code{\link{list_comments}}
+#'
+#'@param comment_id the ID of a comment, which can be easily retrieved with \code{\link{list_replies}}
+#'or \code{\link{get_reply}}
+#'
+#'@param ... further arguments to pass to httr's DELETE
+#'
+#'@seealso \code{\link{get_reply}} or \code{\link{list_replies}}, for retrieving the metadata (including reply
+#'ID) from a specific reply or all replies to a comment, respectively.
+#'
+#'@export
 delete_reply <- function(token, file_id, comment_id, reply_id, ...){
   parameters <- paste0("files/", file_id, "/comments/", comment_id, "/replies/", reply_id)
   results <- driver_delete(parameters, token, ...)
