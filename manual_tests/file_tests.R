b4cd7ee7f2c8fe97b447138056daa4e1d1e4799a
ironholds <ironholds@gmail.com>
2015-02-28 00:18:49 -0500

    more tests

--- manual_tests/file_tests.R
+++ manual_tests/file_tests.R
@@ -27,3 +27,21 @@ test_that("Retrieving metadata for a single simplified file works", {
   expect_true(exists("files")) #It worked?
   expect_false(is.list(file$labels)) #It didn't simplify things.
 })
+
+test_that("File copying works", {
+  file <- list_files(token, max_results = 1)$items[[1]] #Get the original file
+  copy_result <- copy_file(token, file_id = file$id) #Copy!
+  expect_that(paste0("Copy of ", file$title), equals(copy_result$title)) #Expect the title is "Copy of $FOO"
+  expect_that(file$mimeType, equals(copy_result$mimeType)) #Expect they have matching MIME types. Copies, right?
+})
+
+#Insert trashing/untrashing experiments here where they can take advantage of the copy which
+#hasn't yet been consumed by the file deletion testing.
+
+test_that("File deletion works",{
+  file <- list_files(token, max_results = 1)$items[[1]]$id #Get the most recent fileID.
+  result <- delete_file(token, file) #Of course, we just created it with "File copying works", so deleting it is fine.
+  expect_true(result)
+  
+  
+})
