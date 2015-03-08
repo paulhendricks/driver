0d7534e59310b69451d6835330aa8eb36a178ccb
ironholds <ironholds@gmail.com>
2015-03-07 20:02:26 -0500

    fix unit test bugs

--- manual_tests/file_tests.R
+++ manual_tests/file_tests.R
@@ -17,14 +17,14 @@ test_that("Retrieving metadata for a list of simplified files works", {
 test_that("Retrieving metadata for a single un-simplified file works", {
   file_id <- list_files(token, max_results = 1)$items[[1]]$id #Get file_id
   file <- file_metadata(token, file_id = file_id, simplify = FALSE) #Retrieve actual file
-  expect_true(exists("files")) #It worked?
+  expect_true(exists("file")) #It worked?
   expect_true(is.list(file$labels)) #It didn't simplify things.
 })
 
 test_that("Retrieving metadata for a single simplified file works", {
   file_id <- list_files(token, max_results = 1)$items[[1]]$id #Get file_id
   file <- file_metadata(token, file_id = file_id, simplify = TRUE) #Retrieve actual file
-  expect_true(exists("files")) #It worked?
+  expect_true(exists("file")) #It worked?
   expect_false(is.list(file$labels)) #It didn't simplify things.
 })
 
@@ -34,6 +34,7 @@ test_that("File copying works", {
   expect_that(paste0("Copy of ", file$title), equals(copy_result$title)) #Expect the title is "Copy of $FOO"
   expect_that(file$mimeType, equals(copy_result$mimeType)) #Expect they have matching MIME types. Copies, right?
 })
+
 
 #Insert trashing/untrashing experiments here where they can take advantage of the copy which
 #hasn't yet been consumed by the file deletion testing.
